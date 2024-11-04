189c7203cf5f0eebbffddd7d23b5122efc6a9082
Rainara <89259827+itsrainara@users.noreply.github.com>
2024-11-04 20:20:20 -0300

    Update product_route.js

--- backend/src/routes/product_route.js
+++ backend/src/routes/product_route.js
@@ -7,5 +7,6 @@ router.post("/postProduct", produtoController.postProduct);
 router.get("/getAllProducts", produtoController.getProducts);
 router.put("/putProduct/:id", produtoController.putProduct);
 router.delete("/deleteProduct/:id", produtoController.deleteProduct);
+router.get("/expiredAndNearExpiryProducts", produtoController.getExpiredAndNearExpiryProducts);
 
 module.exports = router;
